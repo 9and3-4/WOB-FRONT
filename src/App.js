7732cc6f2c94c960f6414de2d50193ba470b3033
HanbiHeo <hhb0365@gmail.com>
2023-12-07 10:10:30 +0900

    Weekly Calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Schedule from "./page/schedule/Schedule";
+import CalendarComp from "./component/CalendarComp";
 
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/1" element={<CalendarComp />} />
+          <Route path="/2" element={<Schedule />} />
+        </Routes>
+      </Router>
     </>
   );
-};
+}
 
 export default App;
---
+++ src/page/schedule/Schedule.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import moment from "moment";
+import CalendarComp from "../../component/CalendarComp";
+
+const Boards = styled.div`
+  font-size: 17px;
+  font-weight: 600;
+  color: #999999;
+  margin-bottom: 12px;
+`;
+const Subtitle = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const BoardList = styled.h2`
+  text-align: center;
+`;
+const Header = styled.h1`
+  font-size: 40px;
+  font-weight: 700;
+  color: #04bf8a;
+  margin-bottom: 50px;
+  text-align: center;
+`;
+
+const BoardListContainer = styled.div``;
+const Line = styled.div``;
+
+const Schedule = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const onDateSelect = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <>
+      <div>
+        <Boards>
+          <>
+            <Header>WOB</Header>
+          </>
+          <>
+            <CalendarComp onDateSelect={onDateSelect} />
+          </>
+          <>
+            <Subtitle>
+              {selectedDate ? selectedDate.toDate().toDateString() : ""}
+            </Subtitle>
+          </>
+          <BoardListContainer>
+            <Line />
+            <BoardList />
+          </BoardListContainer>
+        </Boards>
+      </div>
+    </>
+  );
+};
+
+export default Schedule;
