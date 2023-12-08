207636c81febeb48114029b144b5dc80a133c127
songwoohee <songwoohee@naver.com>
2023-12-08 12:52:01 +0900

    firebase, kakaomap, searchfilter 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Schedule from "./page/schedule/Schedule";
 import CalendarComp from "./component/CalendarComp";
 import Header from "./layout/Header";
 import GlobalStyle from "./globalStyle";
+import KakaoMap from "./component/Map";
+import SearchComponent from "./component/SearchFilter";
 
 function App() {
   return (
@@ -14,8 +16,8 @@ function App() {
           <Route element={<Header />} >
             <Route path="/" element={<CalendarComp />} />
             <Route path="/schedule" element={<Schedule />} />
+            <Route path="/map" element={<KakaoMap />} />
           </Route>
-  
         </Routes>
       </Router>
     </>
--- src/globalStyle.jsx
+++ src/globalStyle.jsx
@@ -11,13 +11,36 @@ const GlobalStyle = createGlobalStyle`
     --GRAY : #EEEEEE;
   }
 
-
   * {
       box-sizing: border-box;
     }
 
+    html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video, button, textarea, select, input{
+    /* font-family: 'Noto Sans KR', sans-serif;
+  font-family: 'Orbit', sans-serif; */
+  font-family: 'Gowun Dodum', sans-serif;
+  }
+  h2 {
+    font-size: 2.4em;
+  }
+  h3 {
+    font-size: 1.6em;
+  }
+
     .container {
-        margin: 0 auto;
+
     }
 
 `;
