21aa91fb9787ae5470eac10b28ea511b5aa3947f
songwoohee <songwoohee@naver.com>
2023-12-08 16:36:40 +0900

    button component(styled) 최종

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,49 +1,40 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: ${({ color }) => color || 'var(--MINT)'};
+  padding: ${({ size }) =>
+    typeof size === 'number'
+      ? `${size}px`
+      : size === 'small'
+      ? '5px 10px'
+      : size === 'normal'
+      ? '10px 15px'
+      : size === 'large'
+      ? '15px 20px'
+      : size === 'extra-small'
+      ? '3px 6px'
+      : '10px 15px'};
+  border-radius: 25px;
+  border-color: ${({ color }) => color || 'var(--MINT)'};
+  color: ${({ color }) => color || 'var(--BLACK)'};
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+  border: 0;
+  outline: 0;
+`;
 
 const Button = ({ label, onClick, color, size, disabled }) => {
-    const defaultBack = 'var(--MINT)';
-    const defaultLetter = 'var(--BLACK)';
-
-    const paddingValue = 
-    typeof size === 'number' ? `${size}px` :
-    size === 'small' ? '5px 10px' :
-    size === 'normal' ? '10px 15px' :
-    size === 'large' ? '15px 20px' :
-    size === 'extra-small' ? '3px 6px' :
-    '10px 15px'; // 기본값
-
-  const buttonStyle = {
-    backgroundColor: color || defaultBack,
-    padding: paddingValue,
-    borderRadius: '25px',
-    borderColor: 'var(--MINT)',
-    color: color || defaultLetter,
-    cursor: disabled ? 'not-allowed' : 'pointer',
-    opacity: disabled ? 0.5 : 1,   // 비활성버튼 투명해짐 - 요소의 투명도, 0은 완전한 투명(요소 안보임), 1은 완전한 불투명 
-  };
-
-
   return (
-    <button
-      style={buttonStyle}
+    <StyledButton
       onClick={onClick}
       disabled={disabled}
+      color={color}
+      size={size}
     >
       {label}
-    </button>
+    </StyledButton>
   );
-};
-
-Button.propTypes = {
-  label: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-  color: PropTypes.string,
-  size: PropTypes.oneOfType([
-    PropTypes.oneOf(['small', 'normal', 'large', 'extra-small', 'extra-large']),
-    PropTypes.number, // 숫자도 허용
-  ]),
-  disabled: PropTypes.bool,
 };
 
 Button.defaultProps = {
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import Button from "../component/Button";
 import { useNavigate } from "react-router-dom";
 
+
+
 const Main = () => {
     const handleClick = () => {
         alert('Button Clicked!');
@@ -14,7 +16,7 @@ const Main = () => {
             <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Main Page with Button Component</h1>
       <Button label="Small Button" size="small" onClick={() => navigate('/calendarcomp')} />
-      <Button label="Normal Button" size="normal" onClick={handleClick} />
+      <Button label="Normal Button" size="normal" color='gray' onClick={handleClick} />
       <Button label="Large Button" size="large" onClick={handleClick} />
       <Button label="Extra Small Button" size="extra-small" onClick={handleClick} />
       <Button label="Extra Large Button" size="extra-large" onClick={handleClick} />
