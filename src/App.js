72b17d398398ea16512893f003117b702d3eb65b
songwoohee <songwoohee@naver.com>
2023-12-10 17:38:39 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Schedule from "./page/schedule/Schedule";
 import CalendarComp from "./component/CalendarComp";
-import Header from "./layout/Header";
 import GlobalStyle from "./globalStyle";
-import KakaoMap from "./component/Map";
 import Main from "./page/Main";
+import Layout from "./layout/Layout";
 import KakaoMaps from "./page/map/Maps";
+
+
 
 function App() {
   return (
@@ -14,12 +15,10 @@ function App() {
     <GlobalStyle />
       <Router>
         <Routes>
-          <Route element={<Header />} >
-          <Route path="/" element={<Main />} />
-            <Route path="calendarcomp" element={<CalendarComp />} />
+        <Route element={<Layout />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/calendarcomp" element={<CalendarComp />} />
             <Route path="/schedule" element={<Schedule />} />
-            <Route path="/map" element={<KakaoMap />} />
-            {/* 선영지도 */}
             <Route path="/KakaoMap" element={<KakaoMaps />} />
           </Route>
         </Routes>
