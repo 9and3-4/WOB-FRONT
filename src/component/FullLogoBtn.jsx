36d7a5ea6a08bfd9a80f3d78e03fb535f39b2ecd
songwoohee <songwoohee@naver.com>
2023-12-13 16:27:54 +0900

    헤더 로고 다시 수정

--- src/component/FullLogoBtn.jsx
+++ src/component/FullLogoBtn.jsx
@@ -8,17 +8,19 @@ const Container = styled.div`
 `;
 
 const LogoImages = styled.img`
-    width: 300px;
-    height: 300px;
+  width: 300px;
+  height: 300px;
 `;
 
 const FullLogoBth = () => {
-
-    return (
-        <Container>
-            <LogoImages src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/8.png?alt=media&token=1e2813e1-5faf-4090-a86f-5854e8262ae8" alt="logo2"/>
-        </Container>
-    )
+  return (
+    <Container>
+      <LogoImages
+        src="https://firebasestorage.googleapis.com/v0/b/mini-project-1f72d.appspot.com/o/8.png?alt=media&token=1e2813e1-5faf-4090-a86f-5854e8262ae8"
+        alt="logo2"
+      />
+    </Container>
+  );
 };
 
 export default FullLogoBth;
