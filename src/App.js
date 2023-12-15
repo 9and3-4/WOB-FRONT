a2e6fd6655eaaa930fb2616ba12c5135c0bca726
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2023-12-15 20:06:18 +0900

    선영 app.js 수정

--- src/App.js
+++ src/App.js
@@ -15,9 +15,14 @@ import AllMemberInfo from "./page/admin/AllMemberInfo";
 import AllPaymentContent from "./page/admin/AllPaymentContent";
 import AskContent from "./page/admin/AskContent";
 import TestLoginPage from "./page/Login";
-import AdminBoardList from "./page/admin/AdminBoardList";
+import AdminBoardRegistration from "./page/admin/AdminBoardRegistration"
 import AdminBoardDetail from "./page/admin/AdminBoardDetail";
-import InterestEnter from "./page/interestEnter";
+import AdminVisitorStatus from "./page/admin/AdminVisitorStatus";
+import StockRatioByRegion from "./page/admin/StockRatioByRegion";
+import SearchKeyword from "./page/admin/SearchKeyword";
+import PaymentList from "./page/admin/PaymentList";
+import AllPaymentGraph from "./page/admin/AllPaymentGraph";
+import AdminChat from "./page/admin/AdminChat";
 
 function App() {
   return (
@@ -26,7 +31,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<TestLoginPage />} />
-          <Route path="/intereset" element={<InterestEnter />} />
           <Route element={<Layout />}>
             <Route path="/" element={<Main />} />
             <Route path="/calendarcomp" element={<CalendarComp />} />
@@ -44,14 +48,20 @@ function App() {
           <Route path="/Advertising" element={<Advertising />} />
           {/* 전체 게시글 관리 */}
           <Route path="/AllBoardContent" element={<AllBoardContent />} />
-          <Route path="/AdminBoardList" element={<AdminBoardList />} />
-          <Route path="/AdminBoardDetail" element={<AdminBoardDetail />} />
+          <Route path="/AdminBoardRegistration" element={<AdminBoardRegistration/>} />
+          <Route path="/AdminBoardDetail" element={<AdminBoardDetail/>} />
           {/* 전체 회원 관리 */}
           <Route path="/AllMemberInfo" element={<AllMemberInfo />} />
+          <Route path="/AdminVisitorStatus" element={<AdminVisitorStatus />} />
+          <Route path="/StockRatioByRegion" element={<StockRatioByRegion />} />
+          <Route path="/SearchKeyword" element={<SearchKeyword />} />
           {/* 전체 결제 내역 관리 */}
           <Route path="/AllPaymentContent" element={<AllPaymentContent />} />
+          <Route path="/PaymentList" element={<PaymentList />} />
+          <Route path="/AllPaymentGraph" element={<AllPaymentGraph />} />
           {/* 문의하기(Q&A) */}
           <Route path="/AskContent" element={<AskContent />} />
+          <Route path="/AdminChat" element={<AdminChat />} />
         </Routes>
       </Router>
     </>
@@ -59,3 +69,4 @@ function App() {
 }
 
 export default App;
+
