068ddcdb7f423b5eb4c3d0ffdf05f350bd439d20
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2023-12-23 16:48:07 +0900

    선영 app.js수정

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ import Policy from "./page/Setting/Policy";
 import Questions from "./page/Setting/Questions";
 import PasswordChange from "./page/Setting/PasswordChange";
 import Withdrawal from "./page/Setting/Withdrawal";
-import AskContents from "./page/admin/AskContents";
 import SignIn from "./page/login/SignIn";
 import SignUp from "./page/login/SignUp";
 import Address from "./component/Address";
@@ -62,27 +61,6 @@ function App() {
           </Route>
           <Route path="/MyPage" element={<MyPageEdit />} />
           <Route path="/postsubmit" element={<PostSubmit />} />
-          {/* 선영 관리자페이지 */}
-          {/* 관리자 메인 메뉴 페이지 */}
-          <Route path="/AdminMain" element={<AdminMain />} />
-          {/* 광고 관리 */}
-          <Route path="/Advertising" element={<Advertising />} />
-          {/* 전체 게시글 관리 */}
-          <Route path="/AllBoardContent" element={<AllBoardContent />} />
-          <Route
-            path="/AdminBoardRegistration"
-            element={<AdminBoardRegistration />}
-          />
-          {/* 전체 회원 관리 */}
-          <Route path="/AllMemberInfo" element={<AllMemberInfo />} />
-          <Route path="/AdminVisitorStatus" element={<AdminVisitorStatus />} />
-          <Route path="/StockRatioByRegion" element={<StockRatioByRegion />} />
-          <Route path="/SearchKeyword" element={<SearchKeyword />} />
-          {/* 전체 결제 내역 관리 */}
-          <Route path="/AllPaymentList" element={<AllPaymentList />} />
-          {/* 문의하기(Q&A) */}
-          <Route path="/AskContent" element={<AskContent />} />
-          {/* <Route path="/AskContents" element={<AskContents />} /> */}
           {/* 환경설정 */}
           <Route element={<SettingLayout />}>
             <Route path="/Setting" element={<Setting />} />
@@ -99,6 +77,34 @@ function App() {
             <Route path="/Address" element={<Address />} />
           </Route>
           <Route path="/Chatting/:roomId" element={<Chatting />} />
+          <Route>
+            {/* 선영 관리자페이지 */}
+            {/* 관리자 메인 메뉴 페이지 */}
+            <Route path="/AdminMain" element={<AdminMain />} />
+            {/* 광고 관리 */}
+            <Route path="/Advertising" element={<Advertising />} />
+            {/* 전체 게시글 관리 */}
+            <Route path="/AllBoardContent" element={<AllBoardContent />} />
+            <Route
+              path="/AdminBoardRegistration"
+              element={<AdminBoardRegistration />}
+            />
+            {/* 전체 회원 관리 */}
+            <Route path="/AllMemberInfo" element={<AllMemberInfo />} />
+            <Route
+              path="/AdminVisitorStatus"
+              element={<AdminVisitorStatus />}
+            />
+            <Route
+              path="/StockRatioByRegion"
+              element={<StockRatioByRegion />}
+            />
+            <Route path="/SearchKeyword" element={<SearchKeyword />} />
+            {/* 전체 결제 내역 관리 */}
+            <Route path="/AllPaymentList" element={<AllPaymentList />} />
+            {/* 문의하기(Q&A) */}
+            <Route path="/AskContent" element={<AskContent />} />
+          </Route>
           <Route
             path="/CompletePayment/:paymentId"
             element={<CompletePayment />}
