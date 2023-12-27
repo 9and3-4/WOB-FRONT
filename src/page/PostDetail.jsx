4ee820e7fa0a1a5154c00e4d4467008e68f7f6e8
HanbiHeo <hhb0365@gmail.com>
2023-12-27 10:08:55 +0900

    포스트 디테일 덮어써진거 수정해서 다시 커밋..

--- src/page/PostDetail.jsx
+++ src/page/PostDetail.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import PostAxiosApi from "../api/PostAxiosApi";
-import MyPageAxiosApi from "../api/MyPageAxiosApi";
 import { useParams } from "react-router-dom";
 import PostMap from "../component/PostMap";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 import Payment from "../component/Payment";
 import ChatStart from "../component/ChatStart";
+import Modal from "../utils/Modal";
+import MyPageAxiosApi from "../api/MyPageAxiosApi";
 
 const Container = styled.div`
   max-width: 768px;
@@ -76,11 +77,94 @@ const FooterBox = styled.div`
   bottom: 0px;
 `;
 
+const PaymentBtn = styled.button`
+  margin: 10px;
+  width: 90px;
+  height: 45px;
+  background-color: var(--MINT);
+  border-radius: 20px;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const ModalContainer = styled.div`
+  margin: 100px;
+  min-height: 50px;
+  margin: 15px;
+`;
+const ModalSubContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ModalText1 = styled.p`
+  font-size: 24px;
+  white-space: nowrap;
+  width: 30%;
+`;
+const ModalText2 = styled.input`
+  font-size: 24px;
+  width: 60%;
+`;
+
 const PostDetail = ({ categoryImage }) => {
   const { postId } = useParams(); //postId를 url에서 받아옴
   const [post, setPost] = useState("");
   const [postNum, setPostNum] = useState("");
+  const [teaName, setTeaName] = useState("");
+  const [teaPhone, setTeaPhone] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userPhoneNum, setUserPhoneNum] = useState("");
+  const [isUserName, setIsUserName] = useState(false);
+  const [isUserPhoneNum, SetIsUserPhoneNum] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [isPayment, setIsPayment] = useState(false);
 
+  const onModalOpen = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+  const confirmModal = () => {
+    setModalOpen(false);
+  };
+  // 주문자 이름 변수에 저장
+  const onSaveName = (e) => {
+    setUserName(e.target.value);
+    setIsUserName(true);
+    console.log(userName);
+  };
+
+  // 주문자 전화번호 변수에 저장
+  const onSavePhone = (e) => {
+    setUserPhoneNum(e.target.value);
+    SetIsUserPhoneNum(true);
+    console.log(userPhoneNum);
+  };
+
+  // 강사의 이름과 전화번호 받아오기
+  useEffect(() => {
+    const getPostUserInfo = async () => {
+      const rsp = await PostAxiosApi.getPostUserInfo(
+        localStorage.getItem("email")
+      );
+      console.log("회원의 이름과 전화번호 : ", rsp.data[0]);
+      if (rsp.data) {
+        setTeaName(rsp.data[0].name);
+        setTeaPhone(rsp.data[0].phoneNumber);
+      }
+    };
+    getPostUserInfo();
+  }, []);
+
+  // 포스트 정보 가져오기
   useEffect(() => {
     const getPostDetail = async () => {
       console.log("포스트 아이디 : " + postId);
@@ -100,6 +184,7 @@ const PostDetail = ({ categoryImage }) => {
   }, [postId]);
   // if (!post) return <></>;
 
+  //일정추가하기
   const handleAddSchedule = async () => {
     try {
       const email = localStorage.getItem("email"); // 사용자 이메일 가져오기
@@ -147,13 +232,53 @@ const PostDetail = ({ categoryImage }) => {
           {post.type === "normal" && (
             <ChatStart postId={postId}>문의하기</ChatStart>
           )}
-          {post.type === "lesson" && <Payment>결제하기</Payment>}
+          {post.type === "lesson" && (
+            <PaymentBtn onClick={() => onModalOpen()}>결제하기</PaymentBtn>
+          )}
+
           <ContentButton onClick={handleAddSchedule}>일정추가</ContentButton>
         </ButtonBox>
       </ContentBox>
       <FooterBox>
         <Footer />
       </FooterBox>
+      <Modal
+        open={modalOpen}
+        close={closeModal}
+        confirm={confirmModal}
+        type={true}
+        header="주문자 정보를 입력해주세요 !!"
+      >
+        <ModalContainer>
+          <ModalSubContainer>
+            <ModalText1>주문자 성함</ModalText1>
+            <ModalText2 value={userName} onChange={onSaveName}></ModalText2>
+          </ModalSubContainer>
+          <ModalSubContainer>
+            <ModalText1>주문자 연락처</ModalText1>
+            <ModalText2
+              value={userPhoneNum}
+              onChange={onSavePhone}
+              placeholder="하이픈(-) 포함"
+            ></ModalText2>
+          </ModalSubContainer>
+          {isUserName && isUserPhoneNum ? (
+            <Payment
+              setDisabled={false}
+              userName={userName}
+              userPhone={userPhoneNum}
+              postTitle={post.title}
+              postUserName={teaName}
+              fee={post.fee}
+              postPhoneNum={teaPhone}
+            >
+              결제하기
+            </Payment>
+          ) : (
+            <Payment setDisabled={true}>결제하기</Payment>
+          )}
+        </ModalContainer>
+      </Modal>
     </Container>
   );
 };
